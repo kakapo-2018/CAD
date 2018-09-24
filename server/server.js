38bdaf0a7cd193e40cf130f6d7cb77135c04e1e9
ben-vandelaar <benvandelaar@gmail.com>
2018-09-24 17:00:16 +1200

    auth mostly there need to test on proper computer

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 var path = require('path')
 var express = require('express')
+var cors = require('cors');
+var passport = require('passport');
+
 
 var server = express()
 
+server.use(cors('*'));
+
+
+server.use(passport.initialize())
 server.use(express.json())
+// server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/api/auth', require('./routes/auth'))
 
 module.exports = server
