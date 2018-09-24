c65a7e9c0cf360653e63c324d099dcf86d9a1544
Nat <nat_sirisamphan@hotmail.com>
2018-09-24 18:16:08 +1200

    almost I think

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var path = require('path')
 var express = require('express')
 var cors = require('cors');
 var passport = require('passport');
+var bodyParser = require('body-parser');
 
 
 var server = express()
@@ -11,9 +12,15 @@ server.use(cors('*'));
 
 server.use(passport.initialize())
 server.use(express.json())
-// server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
+server.use(bodyParser.json())
+server.use('/api/auth', require('./routes/auth'))
 
 server.use('/api/auth', require('./routes/auth'))
+server.use('/api/users', require('./routes/users'))
+
+server.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = server
